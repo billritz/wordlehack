b8f6b000fb18a80d12970a87d005a3dcdfcab3e5
OnlineUser <billritz@gmail.com>
2022-02-25 08:51:47 -0500

    Add files via upload
    Initial Commit

---
+++ wordlehack.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
+
+wordleStartDate = date(2021, 6, 19)
+
+try:
+    html_text = requests.get('https://www.nytimes.com/games/wordle/index.html', headers={
+        'User-Agent': 'Mozilla/5.0'}).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    hashText = 'window.wordle.hash'
+    hash = soup.find_all('script')
+    jsw = 'var Ma=['
+    for script in hash:
+        if hashText in str(script):
+            hashStart = str(script).index(hashText)+len(hashText)+4
+            hashEnd = str(script)[hashStart:].index("'")
+            hashID = str(script)[hashStart:hashStart+hashEnd]
+
+    if hashID:
+        html_text = requests.get('https://www.nytimes.com/games/wordle/main.' + hashID + '.js', headers={
+            'User-Agent': 'Mozilla/5.0'}).text
+
+        soup = BeautifulSoup(html_text, 'lxml')
+        wordleListStart = str(soup).index(jsw)+len(jsw)
+        wordleListEnd = str(soup)[wordleListStart:].index(']')
+
+        wordleList = list(
+            str(soup)[wordleListStart:wordleListStart+wordleListEnd].replace('"', '').split(","))
+
+        wordleIndex = date.today() - wordleStartDate
+        wordle = wordleList[int(wordleIndex.days)+1]
+
+    if wordle:
+        print("Today's Wordle: {}".format(wordle))
+    else:
+        print("Figure it out yourself.  The script is fucked.")
+
+
+except:
+    print("Figure it out yourself.  The script is fucked.")
